a55bdd2f599f53971d06e68aabf170e92bef7600
JamesBremner <james@ravenspoint.com>
2026-01-19 15:18:46 -0500

    particleSeparation()

--- src/cSPHsim.h
+++ src/cSPHsim.h
@@ -36,4 +36,22 @@ private:
         float mass);
 
     void handle_boundaries(int id);
+
+    /// @brief calculate particle seperation distance
+    /// @param[out] relevant true if particles close enough together to be influential ( i.e < smoothing radius )
+    /// @param[out] vector vector between particles
+    /// @param[out] separation distance between particles
+    /// @param[in] pos1 location first particle
+    /// @param[in] pos2 location second particle
+    ///
+    /// This avoid the square root extraction if particles are far apart
+    /// but in general it is an expensive operation
+    /// so do not make redundant calls to this.
+
+    void particleSeparation(
+        bool &relevant,
+        cxy &vector,
+        float &separation,
+        const cxy &pos1,
+        const cxy &pos2);
 };
